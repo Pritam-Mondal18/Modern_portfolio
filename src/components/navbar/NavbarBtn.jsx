dd4422b543943c4eb1a8887ed701d6121aa86a6a
Pritam-Mondal18 <pritammondal18012003@gmail.com>
2025-04-19 23:06:28 +0530

    responsive navbar

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,7 +4,10 @@ import { FiArrowDownRight } from "react-icons/fi";
 const NavbarBtn = () => {
   return (
     <div className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 cursor-pointer hover:shadow-cyanShadow">
-      Hire Me <FiArrowDownRight />
+      Hire Me
+      <div className="sm:hidden md:block">
+        <FiArrowDownRight />
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "350px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     colors: {
       brown: "#53423e",
       lightBrown: "#645550",
